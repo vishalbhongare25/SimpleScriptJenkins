eb3a6030289fd679b10e243d8607a172dbf4df95
vishal bhongare <vishal.b@servify.in>
2017-10-17 12:48:59 +0530

    Output cahnges

--- src/test/java/Org/demo/ChromeLaunchOnJenkins.java
+++ src/test/java/Org/demo/ChromeLaunchOnJenkins.java
@@ -20,6 +20,7 @@ public class ChromeLaunchOnJenkins {
     @Test(priority=1)
     public void LaunchChromeBrowser() {
     this.driver = new ChromeDriver();
+    System.out.println("Test Chrome ");
   }
     
 }
