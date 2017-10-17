a4fd3fb1fb06b56c9c5ec20d44b3b1c9cddae400
vishal bhongare <vishal.b@servify.in>
2017-10-17 12:40:27 +0530

    My 1st commit

---
+++ src/test/java/Org/demo/ChromeLaunchOnJenkins.java
@@ -0,0 +1,25 @@
+package Org.demo;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class ChromeLaunchOnJenkins {
+	private  WebDriver driver;
+    public WebDriver getDriver() {
+		return driver;
+	}
+
+
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+
+  
+    @Test(priority=1)
+    public void LaunchChromeBrowser() {
+    this.driver = new ChromeDriver();
+  }
+    
+}
---
+++ src/test/java/Org/demo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author vishal
+ *
+ */
+package Org.demo;
